d8de96367dc083e104119640691f3f0007907bd4
Tamiris Shigaki <shigakiendo@gmail.com>
2021-12-04 02:56:28 -0300

    requisito 10: ok

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,16 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  let arraytech = [];
+  for (let tech = 0; tech < array.sort().length; tech += 1) {
+      arraytech.push({
+      tech: array[tech],
+      name: name,
+    });
+  }
+  if (arraytech.length === 0) {
+    return 'Vazio!';
+  }
+  return arraytech;
 }
 
 // Desafio 11
