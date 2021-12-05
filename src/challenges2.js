cc77b9b6e460e800f846ee1494a1a71ca88e4499
Tamiris Shigaki <shigakiendo@gmail.com>
2021-12-05 00:21:08 -0300

    requisito 11: ok

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,9 +14,28 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let repeticao = 0;
+  let numTel = '(' + array[0] + array[1] + ')' + ' ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10]
+
+  if (array.length !== 11) {
+    return "Array com tamanho incorreto.";
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] < 0 || array[i] > 9 || repeticao >= 3) {
+    return "não é possível gerar um número de telefone com esses valores";
+    }
+    repeticao = 0
+    for (let index = 0; index < array.length; index += 1) { //pega a outra posição
+      if (array[i] === array[index]) {
+      repeticao += 1
+      }
+    }
+  }
+  return numTel
 }
+
+console.log (generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
@@ -24,8 +43,8 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  
 }
 
 module.exports = {
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,18 +1,1 @@
-function catAndMouse(mouse, cat1, cat2) {
-  let distanciaCat1 = cat1 - mouse;
-  let distanciaCat2 = cat2 - mouse;
-  let negCat1 = distanciaCat1 * (-1)
-  let negCat2 = distanciaCat2 * (-1)
-  if (distanciaCat1 <= 0 || distanciaCat2 <= 0) {
-    distanciaCat1 * (-1)
-    distanciaCat2 * (-1)
-  }
-  if (distanciaCat1 === distanciaCat2 || distanciaCat1 <= 0 || distanciaCat2 <= 0) {
-    return "os gatos trombam e o rato foge";
-  } else if (distanciaCat1 < distanciaCat2 || distanciaCat1 <= 0 || distanciaCat2 <= 0) {
-   return "cat1";
-  } else if (distanciaCat1 > distanciaCat2 || distanciaCat1 <= 0 || distanciaCat2 <= 0) {
-    return "cat2";
-  }
-}
-console.log (catAndMouse(6, 0, 9));
+for (let i = 0; i < array.length; i += 1) { // pega a posição da array
